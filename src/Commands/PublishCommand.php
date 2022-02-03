1b184a5db4279da6354dc540e74ec6917ec564f0
Maurizio <leMaur@users.noreply.github.com>
2022-02-03 20:36:26 +0100

    refactor: laravel 9 (#1)

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -10,18 +10,17 @@ use Illuminate\Support\Str;
 
 class PublishCommand extends Command
 {
-    protected $signature = 'toolbox:install
-                            {--only=* : Specify which group of files do you want to copy. Possible values are: `static-analysis`, `code-style`, `rector`, `common`, `tests`}
-                            {--T|test-suites : Specify which test suite do you want to install}
-                            {--F|force : Specify if you want to overwrite you existing files}';
+    protected $signature = "toolbox:install
+                            {--only=* : Specify which group of files do you want to copy. Possible values are: `static-analysis`, `code-style`, `refactor`, `common`, `tests`}
+                            {--T|test-suites : Configure Pest and Dusk test suites}
+                            {--S|safe : Specify if you don't want to overwrite your existing files}";
 
-    protected $description = 'Install Toolbox\'s files and tests resources';
+    protected $description = "Install Toolbox's files and tests resources";
 
     private function files(?string $key = null): array
     {
         $files = [
             'static-analysis' => [
-                __DIR__.'/../Commands/stubs/tests/Analysis/AnalysisTest.php.stub' => base_path('tests/Analysis/AnalysisTest.php'),
                 __DIR__.'/../Commands/stubs/phpstan.neon.stub' => base_path('phpstan.neon'),
             ],
 
@@ -29,24 +28,29 @@ class PublishCommand extends Command
                 __DIR__.'/../Commands/stubs/.php-cs-fixer.php.stub' => base_path('.php-cs-fixer.php'),
             ],
 
-            'rector' => [
+            'refactor' => [
+                __DIR__.'/../Commands/stubs/rector-bootstrap.php.stub' => base_path('rector-bootstrap.php'),
                 __DIR__.'/../Commands/stubs/rector.php.stub' => base_path('rector.php'),
-            ],
-
-            'tests' => [
-                __DIR__.'/../Commands/stubs/tests/Browser/Pages/HomePage.php.stub' => base_path('tests/Browser/Pages/HomePage.php'),
-                __DIR__.'/../Commands/stubs/tests/Browser/HomePageTest.php.stub' => base_path('tests/Browser/HomePageTest.php'),
-                __DIR__.'/../Commands/stubs/tests/Feature/HomePageTest.php.stub' => base_path('tests/Feature/HomePageTest.php'),
-                __DIR__.'/../Commands/stubs/tests/Unit/ExampleTest.php.stub' => base_path('tests/Unit/ExampleTest.php'),
-                __DIR__.'/../Commands/stubs/tests/Pest.php.stub' => base_path('tests/Pest.php'),
-                __DIR__.'/../Commands/stubs/phpunit.xml.stub' => base_path('phpunit.xml'),
-                __DIR__.'/../Commands/stubs/.env.dusk.stub' => base_path('.env.dusk'),
             ],
 
             'common' => [
                 __DIR__.'/../Commands/stubs/.editorconfig.stub' => base_path('.editorconfig'),
                 __DIR__.'/../Commands/stubs/.gitignore.stub' => base_path('.gitignore'),
+                __DIR__.'/../Commands/stubs/infection.json.stub' => base_path('infection.json'),
             ],
+
+            'tests' => [
+                __DIR__.'/../Commands/stubs/tests/Analysis/AnalysisTest.php.stub' => base_path('tests/Analysis/AnalysisTest.php'),
+                __DIR__.'/../Commands/stubs/tests/Browser/Pages/HomePage.php.stub' => base_path('tests/Browser/Pages/HomePage.php'),
+                __DIR__.'/../Commands/stubs/tests/Browser/Pages/Page.php.stub' => base_path('tests/Browser/Pages/Page.php'),
+                __DIR__.'/../Commands/stubs/tests/Browser/HomePageTest.php.stub' => base_path('tests/Browser/HomePageTest.php'),
+                __DIR__.'/../Commands/stubs/tests/Feature/HomePageTest.php.stub' => base_path('tests/Feature/HomePageTest.php'),
+                __DIR__.'/../Commands/stubs/tests/Helpers.php.stub' => base_path('tests/Helpers.php'),
+                __DIR__.'/../Commands/stubs/tests/Pest.php.stub' => base_path('tests/Pest.php'),
+                __DIR__.'/../Commands/stubs/phpunit.xml.stub' => base_path('phpunit.xml'),
+                __DIR__.'/../Commands/stubs/.env.dusk.stub' => base_path('.env.dusk'),
+            ],
+
         ];
 
         if (is_null($key)) {
@@ -59,9 +63,18 @@ class PublishCommand extends Command
     public function handle(Filesystem $filesystem): int
     {
         if ($this->option('test-suites')) {
-            $this->call('pest:install', ['--quiet']);
-            $this->call('dusk:install', ['--quiet']);
-            $this->info("Successfully installed [pest, dusk] test suite!");
+            $this->call('pest:install', ['--quiet' => true, '--no-interaction' => true]);
+            $this->info("Successfully installed Pest test suite.");
+
+            $this->call('dusk:install', ['--quiet' => true, '--no-interaction' => true]);
+            $this->info("Successfully installed Dusk test suite.");
+
+            $this->line("");
+            $this->line("You can now delete Example tests and tests/Unit folder with its content.");
+
+            $this->copy($filesystem, ['tests']);
+
+            return 0;
         }
 
         if ($this->option('only')) {
@@ -70,7 +83,16 @@ class PublishCommand extends Command
             return 0;
         }
 
-        $this->copy($filesystem, array_keys($this->files()));
+        $this->call('pest:install', ['--quiet' => true, '--no-interaction' => true]);
+        $this->info("Successfully installed Pest test suite.");
+
+        $this->call('dusk:install', ['--quiet' => true, '--no-interaction' => true]);
+        $this->info("Successfully installed Dusk test suite.");
+
+        $this->copy($filesystem, collect($this->files())->keys()->except('tests')->toArray());
+
+        $this->line("");
+        $this->line("You can now delete Example tests and tests/Unit folder with its content.");
 
         return 0;
     }
@@ -82,18 +104,24 @@ class PublishCommand extends Command
         }
     }
 
-    private function copyFiles(Filesystem $filesystem, array $files, string $group): void
+    private function copyFiles(Filesystem $filesystem, array $files, string $group): int
     {
-        collect($files)->each(function ($published, $original) use ($filesystem) {
-            if (! $this->option('force') && $filesystem->exists($published)) {
-                $this->error("The file [$published] already exists.");
+        collect($files)->each(function ($toPublish, $original) use ($filesystem) {
+            if ($this->option('safe') && $filesystem->exists($toPublish)) {
+                $this->error("The file [$toPublish] already exists.");
+
+                return 1;
             }
 
-            $path = Str::beforeLast($published, '/');
+            $path = Str::beforeLast($toPublish, '/');
             $filesystem->ensureDirectoryExists($path);
-            $filesystem->copy($original, $published);
+            $filesystem->copy($original, $toPublish);
+
+            $this->info("Successfully published $toPublish.");
         });
 
-        $this->info("Successfully published all files for [$group] group!");
+        $this->info("Successfully published all files for [$group] group.");
+
+        return 0;
     }
 }
